fb5b9b2cb9fa9a10b393230de715b8e1ba3a0123
peerapat krairat <peerapat.kra@astrosolutions.co.th>
2024-10-26 20:51:52 +0700

    [update]: toaster with light theme by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 
 import { TanstackQueryProviders } from "@/components/provider/tanstack-query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ export default function RootLayout({
           )}
         >
           <TanstackQueryProviders>
+            <Toaster richColors />
             {children}
           </TanstackQueryProviders>
         </body>
