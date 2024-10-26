b5d82179c8f7ff644d226f589159f3e9800f80f0
peerapat krairat <peerapat.kra@astrosolutions.co.th>
2024-10-26 19:59:43 +0700

    [fix]: improve AuthLayout component
    - Enable scrolling for the main content area
    - Add a footer with a dynamic copyright year

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -17,7 +17,7 @@ const AuthLayout = ({
 
   return (
     <main className="bg-neutral-100 h-screen overflow-hidden">
-      <div className="mx-auto max-w-screen-2xl p-4 h-full">
+      <div className="mx-auto max-w-screen-2xl p-4 h-full flex flex-col gap-2">
         <nav className="flex justify-between items-center">
           <Image
             src="/logo.svg"
@@ -39,9 +39,14 @@ const AuthLayout = ({
             </Link>
           </Button>
         </nav>
-        <div className="flex flex-col justify-center items-center h-full">
+        <div className="flex flex-col md:justify-center items-center h-full overflow-y-auto">
           {children}
         </div>
+        <footer className="flex justify-center items-center border-t border-neutral-200 pt-4">
+          <p className="text-sm text-neutral-500">
+            &copy; {new Date().getFullYear()} All rights reserved.
+          </p>
+        </footer>
       </div>
     </main>
   );
